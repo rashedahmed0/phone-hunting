878dc74b75a383d57f985cb8e7b8848d7719509f
Rialahmed0 <rashedahommod0@gmail.com>
2024-09-16 23:11:09 +0600

    phone hunting

---
+++ script.js
@@ -0,0 +1,32 @@
+let showMobile = async () => {
+    let res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+    let data = await res.json();
+    let phones = data.data
+    // console.log(phones);
+    displayMobile(phones)
+}
+showMobile()
+let displayMobile = (phones) => {
+    // console.log(phones);
+    let mobileContainer = document.getElementById('mobile-container');
+
+    phones.forEach(phone => {
+        let div = document.createElement('div');
+        div.classList = 'card bg-base-100 w-96 shadow-xl';
+        div.innerHTML = `
+              <figure class="px-10 pt-10">
+                    <img src="${phone.image}" />
+                </figure>
+                <div class="card-body items-center text-center">
+                    <h2 class="card-title">${phone.phone_name}!</h2>
+                    <p>There are many variations of passages of available, but the majority have suffered</p>
+                    <h1>$999</h1>
+                    <div class="card-actions">
+                        <button class="btn btn-primary">Show Details</button>
+                    </div>
+                </div>
+        `;
+        mobileContainer.appendChild(div)
+
+    })
+}
