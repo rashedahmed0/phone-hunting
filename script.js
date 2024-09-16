bceaf863ecdc79c519690e934e8c14dec2fb1c42
Rialahmed0 <rashedahommod0@gmail.com>
2024-09-17 00:27:24 +0600

    search option complete

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-let showMobile = async () => {
-    let res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+let showMobile = async (phone) => {
+    let res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${phone}`);
     let data = await res.json();
     let phones = data.data
     // console.log(phones);
     displayMobile(phones)
 }
-showMobile()
 let displayMobile = (phones) => {
     // console.log(phones);
     let mobileContainer = document.getElementById('mobile-container');
+    mobileContainer.innerText = '';
 
     phones.forEach(phone => {
         let div = document.createElement('div');
         div.classList = 'card bg-base-100 w-96 shadow-xl';
         div.innerHTML = `
-              <figure class="px-10 pt-10">
-                    <img src="${phone.image}" />
-                </figure>
-                <div class="card-body items-center text-center">
+        <figure class="px-10 pt-10">
+        <img src="${phone.image}" />
+        </figure>
+        <div class="card-body items-center text-center">
                     <h2 class="card-title">${phone.phone_name}!</h2>
                     <p>There are many variations of passages of available, but the majority have suffered</p>
                     <h1>$999</h1>
@@ -29,4 +29,12 @@ let displayMobile = (phones) => {
         mobileContainer.appendChild(div)
 
     })
+}
+let search = () => {
+    // let searchBtn = document.getElementById('search-btn')
+    let inputField = document.getElementById('input-field');
+    let inputText = inputField.value;
+    console.log(inputText);
+    // inputField.value = '';
+    showMobile(inputText)
 }
